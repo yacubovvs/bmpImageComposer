6a0b934f4c60b4c06a45c604145ad91bfb765c77
Vitaly <yacubovvs@yandex.ru>
2020-11-16 12:58:46 +0300

    adding argv params to application calling

--- image_composer/image_composer.cpp
+++ image_composer/image_composer.cpp
@@ -14,20 +14,30 @@ int main(int argc, char* argv[])
 {
 
     //std::cout << argc << "\n" << argv[1] << "\n" << argv[2] << "\n" << argv[3] << "\n";
-
-    /*
     
-    if (argc < 3) {
+    if (argc < 4) {
         std::cout << "Too few arguments\n";
         std::cout << "Usage:\n";
-        std::cout << "flash_noise_remover.exe [path to image 1 BMP] [path to image 2 BMP] [path to result image BMP]\n";
+        std::cout << "image_composer.exe [full path to image 1 BMP] [full path to image 2 BMP] [full path to result image BMP]\n";
+        //std::cout << "or:\n";
+        //std::cout << "image_composer.exe [path to image 1 BMP] [path to image 2 BMP] [path to result image BMP] [-r]\n";
+        //std::cout << "-r Rotation 180 degrees of 2nd image";
+        std::cout << "both images should have same sizes";
         return 0;
     }
-    */
+    
 
+    FILE* file1     = fopen(argv[1], "rb");
+    FILE* file2     = fopen(argv[2], "rb");
+    FILE* file_out  = fopen(argv[3], "wb");
+    
+
+    
+    /*
     FILE* file1     = fopen("C:\\2\\1\\cam2_2.bmp", "rb");
     FILE* file2     = fopen("C:\\2\\1\\cam1_2.bmp", "rb");
     FILE* file_out  = fopen("C:\\2\\1\\cam_out.bmp", "wb");
+    */
     //FILE* file_out2 = fopen("C:\\2\\1\\cam_out2.hex", "wb");
 
     BITMAPFILEHEADER header1 = readHeader(file1);
@@ -58,15 +68,21 @@ int main(int argc, char* argv[])
         putc(0xff, file_out);
     }*/
 
-    for (int i = 0; i < length; i++) {
-        mass_invert[length - i - 1] = mass[i];
-        //mass_invert[i] = mass[i];
+    //if(argc==5 && argv[4]=="-r"){
+        for (int i = 0; i < length; i++) {
+            mass_invert[length - i - 1] = mass[i];
+            //mass_invert[i] = mass[i];
+        }
+
+        fwrite(mass_invert, 1, length, file_out);
+    /*
+    }else {
+        fwrite(mass, 1, length, file_out);
     }
+    */
 
-    //fwrite(mass, 1, length, file_out);
-    fwrite(mass_invert, 1, length, file_out);
-
-    delete mass;
+    delete []mass;
+    delete []mass_invert;
 
     int val;
     do {
